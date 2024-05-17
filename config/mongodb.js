13ab0059a0c517cbddcc1bc80b19d70658922e1b
Kodiefy <kodiefy.dev@gmail.com>
2024-05-17 21:51:40 +0800

    enhanced middleware and auth models, controllers, routes

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,26 +1,8 @@
 const mongoose = require('mongoose');
-const logger = require('./logger');
-require('dotenv').config()
+const dotenv = require('dotenv');
 
-mongoose.Promise = global.Promise;
+dotenv.config();
 
-mongoose.set('debug', process.env.DEBUG === 'development');
-
-mongoose.connection.on('error', (err) => {
-  logger.error(`MongoDB Connection Error ${err}`);
-});
-
-mongoose.connection.on('connected', () => {
-  logger.info('Connected To DB');
-});
-
-
-/**
- * Connect to mongo db
- * @returns {object} Mongoose connection
- * @public
- */
-exports.Connect = () => {
-  mongoose.connect(process.env.MONGO_CONNECTION);
-  return mongoose.connection;
-};
+mongoose.connect(process.env.MONGO_CONNECTION)
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
